da0ce1791e09bcb99bbde0295001165a8ea45425
vikeshbaid3 <vikeshbaid1@gmail.com>
2023-11-25 22:46:46 +0530

    code shift

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,23 +18,3 @@ terraform {
   }
 }
 
-data "aws_iam_policy_document" "tls_enfore_policy" {
-  statement {
-
-    sid = "1"
-    principals {
-      type = "AWS"
-      identifiers = ["*"]
-    }
-    actions = [
-        "s3:*"
-    ]
-
-    effect = "Allow"
-
-    resources = [
-      aws_s3_bucket.s3_output_bucket.arn,
-      "${aws_s3_bucket.s3_output_bucket.arn}/*",
-    ]
-  }
-}
