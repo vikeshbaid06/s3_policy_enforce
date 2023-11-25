c5e372fb4e8e26e208089d4613419fac855413fd
vikeshbaid3 <vikeshbaid1@gmail.com>
2023-11-25 19:24:09 +0530

    code added for s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,19 +2,19 @@ resource "aws_s3_bucket" "s3_output_bucket" {
     bucket = var.s3_output_bucket
 }
 
-# resource "aws_s3_bucket_public_access_block" "metrics_bucket_block_public" {
-#     bucket = aws_s3_bucket.s3_output_bucket.id
+resource "aws_s3_bucket_public_access_block" "metrics_bucket_block_public" {
+    bucket = aws_s3_bucket.s3_output_bucket.id
 
-#     block_public_acls = true
-#     block_public_policy = true
-#     ignore_public_acls = true
-#     restrict_public_buckets = true
-# }
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
 
-# resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
-#     bucket = aws_s3_bucket.s3_output_bucket.id
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+    bucket = aws_s3_bucket.s3_output_bucket.id
 
-#     rule {
-#         object_ownership = "BucketOwnerPreferred"
-#     }
-# }
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
