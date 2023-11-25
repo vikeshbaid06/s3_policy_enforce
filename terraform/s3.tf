cca4cb1690ccb19fa3db27b787e17bb5a50b3674
vikeshbaid3 <vikeshbaid1@gmail.com>
2023-11-25 17:15:47 +0530

    s3 bucket create

---
+++ terraform/s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "s3_output_bucket" {
+    bucket = var.s3_output_bucket
+    tags = {
+        component = var.component_tag
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "metrics_bucket_block_public" {
+    bucket = aws_s3_bucket.s3_output_bucket.id
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+    bucket = aws_s3_bucket.s3_output_bucket.id
+
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
