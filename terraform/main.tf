9e3e895b212cc28b0c9e0c29186303ae66c54f05
vikeshbaid3 <vikeshbaid1@gmail.com>
2023-11-25 17:15:05 +0530

    main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "4.53.0"
+        }
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    key = "terraform/policy_enfore_bucket/statefile.tfstate"
+    region = "us-east-1"
+  }
+}
