d1c8d6c97eda99de639f8d42601d9ae7404475bf
vikeshbaid3 <vikeshbaid1@gmail.com>
2023-11-25 23:37:41 +0530

    changes public access

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,10 +8,10 @@ resource "aws_s3_bucket" "s3_output_bucket" {
 resource "aws_s3_bucket_public_access_block" "metrics_bucket_block_public" {
     bucket = aws_s3_bucket.s3_output_bucket.id
 
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
