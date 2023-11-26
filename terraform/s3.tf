f6cb3d88abf1ab9cdf834a8e9cfe0ec92c371e09
vikeshbaid3 <vikeshbaid1@gmail.com>
2023-11-26 14:28:08 +0530

    tls enforce policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -45,34 +45,42 @@ resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
 #   policy = data.aws_iam_policy_document.tls_enfore_policy.json
 # }
 
-resource "aws_s3_bucket_policy" "prod_media_bucket" {
+resource "aws_s3_bucket_policy" "tls_enforce_policy" {
     bucket = aws_s3_bucket.s3_output_bucket.id
     policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Principal = "*"
-        Action = [
-          "s3:*",
+        Version = "2012-10-17"
+        Id      = "BUCKET-POLICY"
+        Statement = [
+            {
+                Sid       = "EnforceTls"
+                Effect    = "Deny"
+                Principal = "*"
+                Action    = "s3:*"
+                Resource = [
+                    "${aws_s3_bucket.test_s3_bucket.arn}/*",
+                    "${aws_s3_bucket.test_s3_bucket.arn}",
+                ]
+                Condition = {
+                    Bool = {
+                        "aws:SecureTransport" = "false"
+                    }
+                }
+            },
+            {
+                Sid       = "EnforceProtoVer"
+                Effect    = "Deny"
+                Principal = "*"
+                Action    = "s3:*"
+                Resource = [
+                    "${aws_s3_bucket.test_s3_bucket.arn}/*",
+                    "${aws_s3_bucket.test_s3_bucket.arn}",
+                ]
+                Condition = {
+                    NumericLessThan = {
+                        "s3:TlsVersion": 1.2
+                    }
+                }
+            }
         ]
-        Effect = "Allow"
-        Resource = [
-            aws_s3_bucket.s3_output_bucket.arn,
-            "${aws_s3_bucket.s3_output_bucket.arn}/*"
-        ]
-      },
-      {
-        Sid = "PublicReadGetObject"
-        Principal = "*"
-        Action = [
-          "s3:GetObject",
-        ]
-        Effect   = "Allow"
-        Resource = [
-            aws_s3_bucket.s3_output_bucket.arn,
-            "${aws_s3_bucket.s3_output_bucket.arn}/*"
-        ]
-      },
-    ]
-  })
+    })
 }
